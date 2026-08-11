623b45a71283f28cb4b0fc6db4a5c2bf7101babe
Satyanarayana Karanam <sagarkaranam0922@gmail.com>
2026-08-11 22:14:51 +0530

    Time: 4 ms (82.98%) | Memory: 44.2 MB (82.76%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int value : count) {
+            if (value != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
