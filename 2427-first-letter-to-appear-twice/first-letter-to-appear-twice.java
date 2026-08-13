d6b6c317a4a6fd56622c99d7dfa73fab32fa6303
Satyanarayana Karanam <sagarkaranam0922@gmail.com>
2026-08-13 22:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (87.46%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        boolean[] seen = new boolean[26];
+
+        for (char ch : s.toCharArray()) {
+            if (seen[ch - 'a']) {
+                return ch;
+            }
+
+            seen[ch - 'a'] = true;
+        }
+
+        return ' '; 
+    }
+}
